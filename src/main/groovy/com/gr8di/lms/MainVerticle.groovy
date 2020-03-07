5bbff36a3f89a1e47df60e6580755cd6f93ad18e
kraulain <kraulain@gmail.com>
2020-03-07 22:15:47 +0100

    #000 | deploy all verticles from mainverticle

--- src/main/groovy/com/gr8di/lms/MainVerticle.groovy
+++ src/main/groovy/com/gr8di/lms/MainVerticle.groovy
@@ -1,71 +1,37 @@
 package com.gr8di.lms
 
-import com.gr8di.lms.handlers.HealthCheckHandler
-import com.gr8di.lms.handlers.page.HomePageHandler
+import com.gr8di.lms.database.DatabaseVerticle
 import io.vertx.core.AbstractVerticle
-import io.vertx.core.Future
+import io.vertx.core.DeploymentOptions
 import io.vertx.core.Promise
-import io.vertx.core.http.HttpServer
-import io.vertx.core.json.JsonObject
 import io.vertx.core.logging.Logger
 import io.vertx.core.logging.LoggerFactory
-import io.vertx.ext.jdbc.JDBCClient
-import io.vertx.ext.sql.SQLConnection
-import io.vertx.ext.web.Router
-import io.vertx.ext.web.RoutingContext
-import io.vertx.ext.web.handler.StaticHandler
-import io.vertx.ext.web.templ.freemarker.FreeMarkerTemplateEngine
 
 class MainVerticle extends AbstractVerticle {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class)
 
-    private static final String SQL_CREATE_COURSE_TABLE = "CREATE TABLE IF NOT EXISTS course (id INTEGER IDENTITY PRIMARY KEY, name VARCHAR(255) UNIQUE, description CLOB)"
-    private static final String SQL_GET_COURSE_BY_ID = "SELECT * FROM course WHERE id = ?"
-    private static final String SQL_CREATE_COURSE = "INSERT INTO course VALUES (NULL, ?, ?)"
-    private static final String SQL_UPDATE_COURSE = "UPDATE course SET name = ?, description = ? WHERE id = ?"
-    private static final String SQL_GET_ALL_COURSES = "SELECT * FROM course"
-    private static final String SQL_DELETE_COURSE = "DELETE FROM course WHERE id = ?"
-
     @Override
     public void start(Promise<Void> promise) throws Exception {
-        Future<Void> steps = prepareDatabase().compose({ v -> startHttpServer() })
-        steps.setHandler(promise)
-    }
+        Promise<String> dbVerticleDeployment = Promise.promise()
+        vertx.deployVerticle(new DatabaseVerticle(), dbVerticleDeployment)
 
-    private Future<Void> prepareDatabase() {
-        Promise<Void> promise = Promise.promise()
-        def dbClient = JDBCClient.createShared(vertx, new JsonObject()
-                .put("url", "jdbc:hsqldb:file:db/gr8lms")
-                .put("driver_class", "org.hsqldb.jdbcDriver")
-                .put("max_pool_size", 30))
+        dbVerticleDeployment.future().compose({ id ->
 
-        dbClient.getConnection({ ar ->
-            if (ar.failed()) {
-                LOGGER.error("Could not open a database connection", ar.cause())
-                promise.fail(ar.cause())
+            Promise<String> gatewayVerticleDeployment = Promise.promise()
+            vertx.deployVerticle(
+                    "com.gr8di.lms.gateway.GatewayVerticle",
+                    new DeploymentOptions().setInstances(2),
+                    gatewayVerticleDeployment);
+
+            return gatewayVerticleDeployment.future()
+
+        }).setHandler({ ar ->
+            if (ar.succeeded()) {
+                promise.complete()
             } else {
-                SQLConnection connection = ar.result()
-                connection.execute(SQL_CREATE_COURSE_TABLE, { create ->
-                    connection.close()
-                    if (create.failed()) {
-                        LOGGER.error("Database preparation error", create.cause())
-                        promise.fail(create.cause())
-                    } else {
-                        promise.complete()
-                    }
-                })
+                promise.fail(ar.cause())
             }
         })
-
-        return promise.future()
-    }
-
-
-    private Future<Void> startHttpServer() {
-        Promise<Void> promise = Promise.promise()
-        
-
-        return promise.future()
     }
 }
